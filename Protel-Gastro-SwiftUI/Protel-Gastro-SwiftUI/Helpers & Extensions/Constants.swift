b8352750f300d73a155fa2ae1624014e7cc868ff
ilaydacelikkayaa <ilaydacelikkaya2@gmail.com>
2026-07-16 17:54:06 +0300

    feat: add application-wide constants

---
+++ Protel-Gastro-SwiftUI/Protel-Gastro-SwiftUI/Helpers & Extensions/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Protel-Gastro-SwiftUI
+//
+//  Created by İlayda Çelikkaya on 16.07.2026.
+//
+
+import Foundation
+
+// MARK: - App Constants
+struct Constants {
+    
+    struct API {
+        static let productURL = "https://fakestoreapi.com"
+        static let productsEndpoint = "/products"
+        static let foodURL = "https://www.themealdb.com"
+    }
+}

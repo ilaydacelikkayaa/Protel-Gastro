2e00e2d4364f029bc3ccadac815d0405b34e7661
ilaydacelikkayaa <ilaydacelikkaya2@gmail.com>
2026-07-16 17:54:59 +0300

    feat: create CategoryCellView in SwiftUI

---
+++ Protel-Gastro-SwiftUI/Protel-Gastro-SwiftUI/Scenes/Cells/CategoryCellView.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryCellView.swift
+//  Protel-Gastro-SwiftUI
+//
+//  Created by İlayda Çelikkaya on 16.07.2026.
+//
+
+import SwiftUI
+
+struct CategoryCellView: View {
+    let categoryName: String
+    let isSelected: Bool
+    
+    var body: some View {
+        Text(categoryName)
+            .font(.system(size: 14, weight: .semibold))
+            .foregroundColor(isSelected ? .white : .themeSecondaryText)
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(isSelected ? Color.themeOrange : Color.themeCardBackground)
+            .clipShape(Capsule()) // Tam bir kapsül şekli verir
+    }
+}
